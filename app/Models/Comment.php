69144f0f376abe478290863feaeb52364673ad82
saulobr88 <saulobr88@yahoo.com.br>
2017-12-19 22:45:11 -0300

    Adicionado a notação swagger no model Comment

--- app/Models/Comment.php
+++ app/Models/Comment.php
@@ -4,6 +4,47 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * Class Comment
+ *
+ * @package App\Models
+ *
+ * @SWG\Definition(
+ *     definition="NewComment",
+ *     required={"body", "user_id", "commentable_id", "commentable_type"},
+ *     @SWG\Property(
+ *          property="body",
+ *          type="string",
+ *          description="User's Comment",
+ *          example="lorem ipsum dollor..."
+ *    ),
+ *     @SWG\Property(
+ *          property="user_id",
+ *          type="integer",
+ *          description="User's id in database"
+ *    ),
+ *     @SWG\Property(
+ *          property="commentable_id",
+ *          type="integer",
+ *          description="id in database of resource that will receive the comment"
+ *    ),
+ *     @SWG\Property(
+ *          property="commentable_type",
+ *          type="string",
+ *          description="The Model Class Path of resource that will receive the commnet",
+ *          example="App\Models\Route"
+ *    )
+ * )
+ * @SWG\Definition(
+ *     definition="Comment",
+ *     allOf = {
+ *          { "$ref": "#/definitions/NewComment" },
+ *          { "$ref": "#/definitions/Timestamps" },
+ *          { "required": {"id"} }
+ *     }
+ * )
+ *
+ */
 class Comment extends Model
 {
     /**
