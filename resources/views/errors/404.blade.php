ed3a2432aae7abb7afaafc6cf64138e4559062b8
saulobr88 <saulobr88@yahoo.com.br>
2018-01-21 16:26:07 -0300

    Atualizado as páginas de erros: 404, 403 e 503

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -1,8 +1,16 @@
-@extends('layouts.default')
+@extends('layouts.guest')
 
 @section('content')
-<h3>Ooops</h3>
-<p>404 page not found</p>
-<p><a href="{{ route('index') }}">click here to go back to index</a> </p>
+<div class="tab_container">
+    <input id="tab1" type="radio" name="tabs" checked class="radio_hidden">
+    <label for="tab1" class="head"><i class="fa fa-times-circle"></i><span>Ooops</span></label>
+
+    <div class="contents">
+        <section id="content1" class="tab-content" style="padding-top: 100px; padding-bottom: 200px;">
+            <p>404 page not found</p>
+            <p><a href="{{ route('index') }}">click here to go back to index</a> </p>
+        </section>
+    </div>
+</div>
 
 @endsection
--- resources/views/errors/503.blade.php
+++ resources/views/errors/503.blade.php
@@ -1,7 +1,16 @@
-@extends('layouts.default')
+@extends('layouts.guest')
 
 @section('content')
-<h3>Be back soon</h3>
-<p>We are in maintenance mode, sorry.</p>
+<div class="tab_container">
+    <input id="tab1" type="radio" name="tabs" checked class="radio_hidden">
+    <label for="tab1" class="head"><i class="fa fa-cogs"></i><span>Be back soon</span></label>
+
+    <div class="contents">
+        <section id="content1" class="tab-content" style="padding-top: 100px; padding-bottom: 200px;">
+            <p>We are in maintenance mode, sorry.</p>
+        </section>
+    </div>
+</div>
 
 @endsection
+
